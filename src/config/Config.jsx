ba231a94f60272f9c64560b43d6416aff3b23b55
Tigran <gevorgyant05@gmail.com>
2023-02-22 11:22:31 +0400

    correcting firebase

--- src/config/Config.jsx
+++ src/config/Config.jsx
@@ -1,37 +1,37 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  doc,
-  setDoc,
-  updateDoc,
-  deleteField,
-  deleteDoc,
-} from "firebase/firestore";
-import "firebase/compat/storage";
-import { initializeApp } from "firebase/app";
-import { v4 as uuidv4 } from "uuid";
-import { getStorage } from "firebase/storage";
+	getFirestore,
+	collection,
+	getDocs,
+	addDoc,
+	doc,
+	setDoc,
+	updateDoc,
+	deleteField,
+	deleteDoc,
+} from 'firebase/firestore';
+import 'firebase/compat/storage';
+import { initializeApp } from 'firebase/app';
+import { v4 as uuidv4 } from 'uuid';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCt1AdV22OrgmirQNOn54vFVbkywPvziNg",
-  authDomain: "online-shop-e417e.firebaseapp.com",
-  databaseURL:
-    "https://online-shop-e417e-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "online-shop-e417e",
-  storageBucket: "online-shop-e417e.appspot.com",
-  messagingSenderId: "692023712078",
-  appId: "1:692023712078:web:01985f1d1b25b9fe45261f",
-  measurementId: "G-MJK1KCZKR1",
+	apiKey: 'AIzaSyDAnmGYpUbn78IfF081K5BtIy8czQn6xc8',
+	authDomain: 'bork-clone.firebaseapp.com',
+	databaseURL: 'https://bork-clone-default-rtdb.firebaseio.com',
+	projectId: 'bork-clone',
+	storageBucket: 'bork-clone.appspot.com',
+	messagingSenderId: '881131314599',
+	appId: '1:881131314599:web:aa1d90e71aa15230c2aa82',
+	measurementId: 'G-T95BJG1T3Y',
 };
 
-firebase.initializeApp(firebaseConfig);
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 
@@ -52,66 +52,66 @@ export const storage = getStorage(app);
 //   .catch((err) => console.log(err.message));
 
 export const addUsersFirebase = (userName, email, password) => {
-  setDoc(doc(db, "SignedUpUsers", uuidv4()), {
-    userName: userName,
-    email: email,
-    password: password,
-  });
+	setDoc(doc(db, 'SignedUpUsers', uuidv4()), {
+		userName: userName,
+		email: email,
+		password: password,
+	});
 };
 
 export const addImagesFirebase = (name, price, src, id, email, categories) => {
-  setDoc(doc(db, "Images", id), {
-    src: src,
-    price: price,
-    name: name,
-    email: email,
-    categories: categories,
-  });
-  setDoc(doc(db, categories, id), {
-    src: src,
-    price: price,
-    name: name,
-    email: email,
-  });
+	setDoc(doc(db, 'Images', id), {
+		src: src,
+		price: price,
+		name: name,
+		email: email,
+		categories: categories,
+	});
+	setDoc(doc(db, categories, id), {
+		src: src,
+		price: price,
+		name: name,
+		email: email,
+	});
 };
 
 export const addBasket = (email) => {
-  setDoc(doc(db, "Basket", email), {});
+	setDoc(doc(db, 'Basket', email), {});
 };
 export const addFavorite = (email) => {
-  setDoc(doc(db, "Favorite", email), {});
+	setDoc(doc(db, 'Favorite', email), {});
 };
 
 export const addItemFirebase = (card, email, id) => {
-  updateDoc(doc(db, "Basket", email), {
-    [id]: card,
-  });
+	updateDoc(doc(db, 'Basket', email), {
+		[id]: card,
+	});
 };
 export const addItemFirebaseFavorite = (card, email, id) => {
-  updateDoc(doc(db, "Favorite", email), {
-    [id]: card,
-  });
+	updateDoc(doc(db, 'Favorite', email), {
+		[id]: card,
+	});
 };
 
 export const changeCount = (email, id, card) => {
-  updateDoc(doc(db, "Basket", email), {
-    [id]: card,
-  });
+	updateDoc(doc(db, 'Basket', email), {
+		[id]: card,
+	});
 };
 
 export const deleteItemFirebase = (email, id) => {
-  updateDoc(doc(db, "Basket", email), {
-    [id]: deleteField(),
-  });
+	updateDoc(doc(db, 'Basket', email), {
+		[id]: deleteField(),
+	});
 };
 export const deleteItemFirebaseFavorite = (email, id) => {
-  updateDoc(doc(db, "Favorite", email), {
-    [id]: deleteField(),
-  });
+	updateDoc(doc(db, 'Favorite', email), {
+		[id]: deleteField(),
+	});
 };
 export const deleteAddedItemFirebaseImages = (id) => {
-  deleteDoc(doc(db, "Images", id));
+	deleteDoc(doc(db, 'Images', id));
 };
 export const deleteAddedItemFirebaseCategories = (categories, id) => {
-  deleteDoc(doc(db, categories, id));
+	deleteDoc(doc(db, categories, id));
 };
--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-const firebaseConfig = {
-	apiKey: 'AIzaSyCt1AdV22OrgmirQNOn54vFVbkywPvziNg',
-	authDomain: 'online-shop-e417e.firebaseapp.com',
-	databaseURL:
-		'https://online-shop-e417e-default-rtdb.europe-west1.firebasedatabase.app',
-	projectId: 'online-shop-e417e',
-	storageBucket: 'online-shop-e417e.appspot.com',
-	messagingSenderId: '692023712078',
-	appId: '1:692023712078:web:01985f1d1b25b9fe45261f',
-	measurementId: 'G-MJK1KCZKR1',
-};
+// const firebaseConfig = {
+// 	apiKey: 'AIzaSyCt1AdV22OrgmirQNOn54vFVbkywPvziNg',
+// 	authDomain: 'online-shop-e417e.firebaseapp.com',
+// 	databaseURL:
+// 		'https://online-shop-e417e-default-rtdb.europe-west1.firebasedatabase.app',
+// 	projectId: 'online-shop-e417e',
+// 	storageBucket: 'online-shop-e417e.appspot.com',
+// 	messagingSenderId: '692023712078',
+// 	appId: '1:692023712078:web:01985f1d1b25b9fe45261f',
+// 	measurementId: 'G-MJK1KCZKR1',
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
